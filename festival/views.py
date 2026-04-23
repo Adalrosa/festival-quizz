0b7a54f0c1e45864e447e768f7f948ffc8ece46b
lp1 <160870127+Adalrosa@users.noreply.github.com>
2026-04-23 15:47:41 +0000

    Views finalizadas

--- festival/views.py
+++ festival/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render
-from .models import Palco
-
+from .models import Dia, Palco, Concerto
 
 def index_view(request):
     return render(request, 'festival/index.html')
 
-
 def dias_view(request):
     dias = Dia.objects.all() 
-
     context = {'dias': dias}
-
     return render(request, 'festival/dias.html', context)
 
-
-
 def concerto_view(request, id):
-    concerto = 
-
+   
+    concerto = Concerto.objects.get(id=id)
     context = {'concerto': concerto}
-
     return render(request, 'festival/concerto.html', context)
